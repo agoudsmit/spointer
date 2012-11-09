3a41af32b824e507207b7ff00ee8337594058b88
Peter StJ <regardingscot@gmail.com>
2012-11-09 17:03:09 +0200

    Fix game literal object structure to reflect the new state on server.

--- js/client/ds.Game.js
+++ js/client/ds.Game.js
@@ -4,6 +4,9 @@
 
 goog.provide('spo.ds.Game');
 
+goog.require('goog.i18n.DateTimeFormat');
+goog.require('goog.i18n.DateTimeParse');
+goog.require('pstj.date.utils');
 goog.require('pstj.ds.ListItem');
 
 /**
@@ -26,6 +29,29 @@ spo.ds.Game.prototype.isPaused = function() {
 };
 
 /**
+ * @inheritDoc
+ */
+spo.ds.Game.prototype.getProp = function(property) {
+  if (property == spo.ds.Game.Property.START_TIME ||
+    property == spo.ds.Game.Property.SAVED_GAME_TIME ||
+    property == spo.ds.Game.Property.SAVED_REAL_TIME) {
+    var timestring = goog.base(this, 'getProp', property);
+    return new Date(timestring);
+  }
+  return goog.base(this, 'getProp', property);
+};
+
+/**
+ * Gets a formatted version of the start date (for game details)
+ * @return {string} The formmated date as per the spo.ds.Game.Formatting rules.
+ */
+spo.ds.Game.prototype.getFormatedStartDate = function() {
+  var starttime = this.getProp(spo.ds.Game.Property.START_TIME);
+  return pstj.date.utils.renderTime(starttime,
+    spo.ds.Game.Formatting.DATE_ONLY);
+};
+
+/**
  * Provides the names for the object literal.
  * @enum {string}
  */
@@ -37,5 +63,29 @@ spo.ds.Game.Property = {
   START_TIME: 'game_started_date',
   IS_LOCKED: 'is_locked',
   PLAYERS_COUNT: 'players_count',
-  STATUS: 'state_id'
+  STATUS: 'state_id',
+  SAVED_GAME_TIME: 'last_saved_game_time',
+  SAVED_REAL_TIME: 'last_saved_real_time'
+
 };
+
+/**
+ * @enum {string}
+ */
+spo.ds.Game.Formatting = {
+  DATE_ONLY: 'mm/dd/yyyy',
+  TIME_ONLY: 'hh:xx'
+};
+
+/**
+ * The formatter fot use when using those dates.
+ * @type {goog.i18n.DateTimeFormat}
+ */
+spo.ds.Game.DateFormatter = new goog.i18n.DateTimeFormat("MM'/'dd'/'yyyy");
+
+/**
+ * The parser to use when parsing date time from game edits.
+ * @type {goog.i18n.DateTimeParse}
+ */
+spo.ds.Game.DateParser = new goog.i18n.DateTimeParse(
+  "MM'/'dd'/'yyyy");
