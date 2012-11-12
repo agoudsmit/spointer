57fe16ea737929bc2051030e0799f90ee9cf3811
Peter StJ <regardingscot@gmail.com>
2012-11-12 16:59:49 +0200

    Calculate server time in milliseconds to match Date object system wide.

--- js/client/ds.ServerTimeProvider.js
+++ js/client/ds.ServerTimeProvider.js
@@ -1,38 +1,44 @@
 goog.provide('spo.ds.STP');
 
-goog.require('pstj.ds.TimeProvider');
-
 /**
  * Global time provider that accounts for server time shift.
+ * Use it if one needs to have access to the exact time on the server
+ * in cases where the server might be in another time zone and you do not want
+ * to fight with the UTC.
+ *
  * @constructor
- * @extends {pstj.ds.TimeProvider}
  */
 spo.ds.STP = function() {};
 
 /**
- * Use this only for debugging.
- * @return {number} The server-client time delta.
+ * Informational method, getter for the delta currently used.
+ *
+ * @return {number} Returns the delta time between the server and the
+ *                          client time in milliseconds.
  */
 spo.ds.STP.prototype.getDelta = function() {
   return  this.delta_;
 };
 
 /**
- * Returns the current server time in PHP time (seconds in unix epoch).
- * @return {number} The current time on the server.
+ * Returns the current server time as milliseconds from 1970 UTC.
+ *
+ * @return {number} The current time on the server as calculated by the
+ *                      initially provided delta in milliseconds.
  */
 spo.ds.STP.prototype.getServerTime = function(time) {
-  if (!goog.isNumber(time)) time = (goog.now() / 1000) << 0;
+  if (!goog.isNumber(time)) time = goog.now();
   return time - this.delta_;
 };
 
 /**
  * Updates the server time the provider knows about. Use it to sync
  * to the server time once it is provided.
- * @param  {number} server_time The server time in PHP seconds unix time.
+ *
+ * @param {number} server_time The server time in milliseconds.
  */
 spo.ds.STP.prototype.setServerTime = function(server_time) {
-  this.delta_ = ((goog.now() / 1000) << 0) - server_time;
+  this.delta_ = goog.now() - server_time;
 };
 
 goog.addSingletonGetter(spo.ds.STP);
