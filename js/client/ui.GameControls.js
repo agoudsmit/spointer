35861dca6c47222059e79bf839b0875843363356
Peter StJ <regardingscot@gmail.com>
2012-11-12 17:00:22 +0200

    Fix JSDoc

--- js/client/ui.GameControls.js
+++ js/client/ui.GameControls.js
@@ -13,6 +13,9 @@ goog.require('spo.control.EventType');
 goog.require('spo.template');
 
 /**
+ * Provides the control elements for a game. Those are the buttons that
+ * are on top of the game widget.
+ *
  * @constructor
  * @extends {goog.ui.Component}
  * @param {goog.dom.DomHelper=} odh Optional dom helper.
@@ -42,8 +45,23 @@ spo.ui.GameControls.prototype.decorateInternal = function(element) {
   this.createControls_(controls);
 };
 
+/**
+ * @inheritDoc
+ */
+spo.ui.GameControls.prototype.enterDocument = function() {
+  this.getHandler().listen(this,
+    goog.ui.Component.EventType.ACTION, this.handleControlAction_);
+};
+
+
+/**
+ * Sets the play state of the control. This is if the game is in play
+ * state the pause buttton should be active, in pause state the play
+ * button should be active.
+ *
+ * @param {boolean} play True if the game is in play state.
+ */
 spo.ui.GameControls.prototype.setPlayState = function(play) {
-  console.log('Game is playable', play)
   if (play) {
     this.getChildAt(0).getElement().style.display = 'block';
     this.getChildAt(1).getElement().style.display = 'none';
@@ -54,6 +72,12 @@ spo.ui.GameControls.prototype.setPlayState = function(play) {
   }
 };
 
+/**
+ * Sets the edit state of the control. This is if the game is being edited
+ * the edit button should disapear.
+ *
+ * @param {boolean} edit true if the game is in edit mode.
+ */
 spo.ui.GameControls.prototype.setEditState = function(edit) {
   if (edit) {
     this.getChildAt(4).getElement().style.display = 'none';
@@ -63,7 +87,9 @@ spo.ui.GameControls.prototype.setEditState = function(edit) {
 };
 
 /**
- * Create coustom controls from the class-ed elements.
+ * Create coustom controls from the class-ed elements. It expects all elements
+ * that need decoration. The elements should have the data-action attribute.
+ *
  * @param {goog.array.ArrayLike} els The elements to work with.
  * @private
  */
@@ -73,7 +99,8 @@ spo.ui.GameControls.prototype.createControls_ = function(els) {
 
 /**
  * Creates a custom button control from an element. The action of the button
- * should be contained in its data set.
+ * should be contained in its data set (data-action).
+ *
  * @param  {Element} el The Element to make a child control of.
  * @private
  */
@@ -88,9 +115,11 @@ spo.ui.GameControls.prototype.createControl_ = function(el) {
 /**
  * Handler function for the ACTION event of the custom buttons. The handler
  * will receive the action event from the buttons but is bound to the parent.
- * @param  {goog.events.Event} e The goog.ui.Component.EventType.ACTION event
- * coming from the buttons that are children of this control.
+ *
  * @private
+ * @param  {goog.events.Event} e The goog.ui.Component.EventType.ACTION event
+ *                               coming from the buttons that are children of
+ *                               this control.
  */
 spo.ui.GameControls.prototype.handleControlAction_ = function(e) {
   var target = e.target.getElement();
@@ -102,9 +131,10 @@ spo.ui.GameControls.prototype.handleControlAction_ = function(e) {
 
 /**
  * Handles a named action coming from the control buttons.
- * @param  {?string} action The name of the action as recived by the html data
- * set.
+ *
  * @private
+ * @param  {?string} action The name of the action as recived by the html data
+ *                          set.
  */
 spo.ui.GameControls.prototype.handleNamedAction_ = function(action) {
   if (goog.isString(action) && !goog.string.isEmpty(action)) {
@@ -144,12 +174,3 @@ spo.ui.GameControls.prototype.handleNamedAction_ = function(action) {
     }
   }
 };
-
-/**
- * @inheritDoc
- */
-spo.ui.GameControls.prototype.enterDocument = function() {
-  this.getHandler().listen(this,
-    goog.ui.Component.EventType.ACTION, this.handleControlAction_);
-};
-
